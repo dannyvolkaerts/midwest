db4f7c35c84b5e67a7e8e59b2430a8fce0bc5ce7
Danny Volkaerts <u0073950@ucll.be>
2019-06-14 15:02:20 +0200

    tart :wq

---
+++ R/hello.R
@@ -0,0 +1,24 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+midwestt <- function(){
+  library(ggplot2)
+  library(plotly)
+  gg<-ggplot(midwest, aes(x=area, y=poptotal)) + geom_point()
+  ggp<-ggplotly(gg)
+  htmlwidgets::saveWidget(widgetframe::frameableWidget(ggp),'ggplotly_plot_selfcontained.html', selfcontained = TRUE)
+  rawhtml <- readBin('ggplotly_plot_selfcontained.html', raw(), file.info('ggplotly_plot_selfcontained.html')$size)
+  return(rawhtml)
+}
